8d9a1861a20defcb96605120938a4c15306db71c
Maximilian Kemmerer <Maxi.Kemmerer@gmx.de>
2019-08-30 13:51:16 +0200

    Small style corrections

--- ping_test.py
+++ ping_test.py
@@ -15,7 +15,7 @@ file_name = "ping_test.csv"
 direc = "Data"
 file_path = direc+"/"+file_name
 
-if not "ping_test.csv" in os.listdir("./"+direc):
+if "ping_test.csv" not in os.listdir("./"+direc):
     df_my_ping = pd.DataFrame(columns=["date", "min", "max", "avg", "url"])
 else:
     df_my_ping = pd.read_csv(file_path, index_col=0)
@@ -23,23 +23,22 @@ else:
 
 def get_ping_as_df(url):
     my_ping = ping(url)
-    return pd.DataFrame({"date":[time.strftime("%d.%m.%Y %H:%M:%S",time.localtime())],
-                        "min":[my_ping.rtt_min_ms],
-                        "max": [my_ping.rtt_max_ms],
-                        "avg":[my_ping.rtt_avg_ms],
-                        "url":[url]})
-
+    return pd.DataFrame({"date": [time.strftime("%d.%m.%Y %H:%M:%S", time.localtime())],
+                         "min": [my_ping.rtt_min_ms],
+                         "max": [my_ping.rtt_max_ms],
+                         "avg": [my_ping.rtt_avg_ms],
+                         "url": [url]})
 
 
 # Function gets executed when keyboard interrupt occures
-def keyboardInterruptHandler(signal, frame):
+def keyboard_interrupt_handler(signal, frame):
     print("Keyboard interrupt - saving files to ping_test.csv.")
     df_my_ping.to_csv(file_path)
     exit(0)
 
 
 # Execution of infinit loop and catch if keyboard interrupts --> afterwarts persistent storage as csv
-signal.signal(signal.SIGINT, keyboardInterruptHandler)
+signal.signal(signal.SIGINT, keyboard_interrupt_handler)
 
 print("#### Currently analyzing the network ####")
 while True:
