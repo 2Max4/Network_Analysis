8152d072f9bf1c28c13b2834c24b284f4e4eb1f4
Harshal <37841724+harshalkh@users.noreply.github.com>
2019-10-26 13:29:23 +0530

    Email Report generation from html files
    Email Report generation from html files from webpage/figures/

--- send_email.py
+++ send_email.py
@@ -13,17 +13,42 @@ message["From"] = sender_from_email
 message["To"] = receiver_to_email
 
 # Create the HTML version of email content message
-email_content_html = """\
-<html>
-  <body>
-    <p>Hi,<br>
-       Here is your Network Analysis Report.<br>
-    </p>
-  </body>
-</html>
-"""
+with open("webpage/mail_format.html") as report_file:
+    raw_html = report_file.readlines()
+    email_content_html_format = """""".join(raw_html)
+
+#network_speeds
+network_speeds_report_file = "webpage/figures/fig_network_speeds_under_upper_bound.html"
+network_speeds_content_html = """"""
+try:
+    with open(network_speeds_report_file) as report_file:
+        raw_html = report_file.readlines()
+        network_speeds_content_html = """""".join(raw_html)
+except IOError:
+    print('File is not accessible - ' + network_speeds_report_file)
+
+#ping_times_w_outliers
+ping_times_w_outliers_report_file = "webpage/figures/fig_ping_times_with_extreme_outliers.html"
+ping_times_w_outliers_content_html = """"""
+try:
+    with open(ping_times_w_outliers_report_file) as report_file:
+        raw_html = report_file.readlines()
+        ping_times_w_outliers_content_html = """""".join(raw_html)
+except IOError:
+    print('File is not accessible - ' + ping_times_w_outliers_content_html)
+
+#ping_times_wo_outliers
+ping_times_wo_outliers_report_file = "webpage/figures/fig_ping_times_without_extreme_outliers.html"
+ping_times_wo_outliers_content_html = """"""
+try:
+    with open(ping_times_wo_outliers_report_file) as report_file:
+        raw_html = report_file.readlines()
+        ping_times_wo_outliers_content_html = """""".join(raw_html)
+except IOError:
+    print('File is not accessible - ' + ping_times_wo_outliers_report_file)
 
 # Convert to MIMEText objects
+email_content_html = email_content_html_format %(network_speeds_content_html, ping_times_w_outliers_content_html, ping_times_wo_outliers_content_html)
 multipart = MIMEText(email_content_html, "html")
 
 # Add HTML part to MIMEMultipart message
