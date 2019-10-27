29af51727c43557280fb3f0fac1cb763cc7468fb
Maximilian Kemmerer <Maxi.Kemmerer@gmx.de>
2019-10-27 18:36:40 +0100

    added cli to start network test from cmd

---
+++ network_cli.py
@@ -0,0 +1,22 @@
+from threading import Thread
+import network_test_class as ntc
+from send_email import SendEmail
+from getpass import getpass
+
+
+if __name__ == '__main__':
+
+    # Define all parameters in order to send frequent email-reports
+    email_report = SendEmail()
+    email_report.sender_from_email = input("Type email address for login:")
+    email_report.receiver_to_email = input("Type email address for receiving the report:")
+    email_report.password = getpass("Type your password for login to email account and hit enter:")
+
+    # Start thread which does the actual network analysis
+    Thread(target=ntc.InteractivePlots()).start()
+
+    # Start thread which sends frequent email reports
+    Thread(target=email_report.send_weekly_report())
+
+
+
