831001630c0d65def72b411f133793f5d178ed4a
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2019-10-29 02:33:58 +0530

    added buttons and connected with events

--- GUI/main_gui.py
+++ GUI/main_gui.py
@@ -16,17 +16,35 @@ class Screen(QDialog):
     def createTestLayout(self):
         testLabel = QLabel("Select the test you want to perform: ")
         pingTestCheckbox = QCheckBox("&Ping Test")
+        pingTestCheckbox.setChecked(self.doPingTest)
         speedTestCheckbox = QCheckBox("&Speed Test")
+        speedTestCheckbox.setChecked(self.doSpeedTest)
         testLayout = QHBoxLayout()
         testLayout.addWidget(testLabel)
         testLayout.addWidget(pingTestCheckbox)
         testLayout.addWidget(speedTestCheckbox)
         return testLayout
 
+    def creatActionButtonsLayout(self):
+        startTestButton = QPushButton("Start Test")
+        startTestButton.clicked.connect(self.startTest)
+        endTestButton = QPushButton("End Test")
+        endTestButton.clicked.connect(self.endTest)
+        generateGraphButton = QPushButton("Generate Graph")
+        generateGraphButton.clicked.connect(self.generateGraph)
+
+        actionButtonsLayout = QHBoxLayout()
+        actionButtonsLayout.addWidget(startTestButton)
+        actionButtonsLayout.addWidget(endTestButton)
+        actionButtonsLayout.addWidget(generateGraphButton)
+        return actionButtonsLayout
+
     def generateScreen(self):
         testLayout = self.createTestLayout()
+        actionButtonsLayout = self.creatActionButtonsLayout()
         mainLayout = QGridLayout()
         mainLayout.addLayout(testLayout, 0, 0, 1, 2)
+        mainLayout.addLayout(actionButtonsLayout, 1, 0, 1, 2)
         self.setLayout(mainLayout)
         self.setWindowTitle("Network Test")
         QApplication.setStyle(QStyleFactory.create('Fusion'))
@@ -37,8 +55,8 @@ class Screen(QDialog):
     def endTest(self):
         print("Test Ended")
 
-    def display(self):
-        pass
+    def generateGraphButton(self):
+        print("Generate Graph Started")
 
 import sys
 app = QApplication(sys.argv)
